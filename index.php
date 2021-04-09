c24e47c2ebef549d3d8647f4ba1b5959ee0fd5e8
Gabriel1424 <gtoledobento@hotmail.com>
2021-04-09 20:59:50 -0300

    Up Contents

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instagram</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="./img/favicon.ico">
+</head>
+<body>
+   <div class="instagram-wrapper">
+        <div class="instagram-phone">
+            <img src="./img/instagram-celular.png" alt="CellPhone">
+        </div>
+
+        <div class="instagram-continue">
+            <div class="group">
+                <img src="./img/instagram-logo.png" class="instagram-logo" alt="InstaLogo">
+                <div class="profile-photo">
+                    <img src="./img/perfil-instagram.jpg" alt="ProfilePicture">
+                </div>
+                <a href="https://www.instagram.com" class="instagram-login">Continue como gtoledob_</a>
+                <p class="not-account">Não é gtoledob_?
+                <span class="link-blue">Trocar de conta</span>
+                </p>
+            </div>
+           <!-- <div class="group">
+                <p class="not-account">Não é gtoledob_?</p>
+                <p class="not-account">
+                    <span class="link-blue">Alternar conta</span> 
+                    ou
+                    <span class="link-blue">Inscreva-se</span>
+                </p>
+            </div>
+            <div class="get-the-app">
+                <p class="get-app">Baixe o aplicativo</p>
+                <div class="download">
+                    <a href="#" class="app-download"></a>
+                    <a href="#" class="app-download"></a>
+                </div>
+
+            </div> -->
+        </div>
+    </div>
+</body>
+</html>
